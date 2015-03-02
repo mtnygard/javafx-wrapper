1e953f6099bca941ffddd1f06e09949a64d8de1a
Michael Nygard <mtnygard@gmail.com>
2015-03-01 22:10:47 -0600

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject javafx-wrapper "0.1.1"
+(defproject javafx-wrapper "0.1.2-SNAPSHOT"
   :description  "Very thin wrapper around JavaFX objects"
 
   :url          "https://github.com/mtnygard/javafx-wrapper"
