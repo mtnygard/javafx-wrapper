87b9ef214fc6a239e9181d2f2c4a16bd46aa32c6
Michael T. Nygard <mtnygard@gmail.com>
2017-08-03 10:12:54 -0500

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.macro "0.1.2"]]
 
   :plugins      [[codox "0.8.10"]]
